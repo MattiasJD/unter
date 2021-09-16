af2d4291d9c60e51c699e96e1a63db729bb0a2ab
Mattias Justin Demjan <mattias.demjan@student.spsmb.cz>
2021-09-16 14:20:41 +0200

    first ocmmit - test

---
+++ src/bruh/Main.java
@@ -0,0 +1,27 @@
+package bruh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt!");
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        String aa = args[2];
+        int c = 0;
+        switch (aa){
+            case "n":
+                c = a*b;
+                break;
+            case "s":
+                c = a+b;
+                break;
+            case "o":
+                c = a-b;
+                break;
+            case "d":
+                c= a/b;
+                break;
+        }
+        System.out.println(c);
+    }
+}
---
+++ src/commands/Main.java
@@ -0,0 +1,7 @@
+package commands;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
