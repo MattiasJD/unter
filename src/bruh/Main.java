23ae8bdd040f7bf4b156291940afc3df1ca0a7f3
mattias.demjan <maty.demjan@seznam.cz>
2021-09-16 14:45:39 +0200

    yes

--- src/bruh/Main.java
+++ src/bruh/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Welt!");
         /* omegalol */
+        /* funny, isn't it? */
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         String aa = args[2];
